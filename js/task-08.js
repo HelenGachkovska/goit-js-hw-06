d576332be8c7be906298c460064338987081d3fd
HelenGachkovska <ogachkovska@gmail.com>
2023-02-28 21:10:15 +0200

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", hendlerFormSubmit);
+
+function hendlerFormSubmit(event) {
+  event.preventDefault();
+
+  const formElement = event.currentTarget.elements;
+
+  if (formElement.email.value === "" || formElement.password.value === "") {
+    alert("всі поля повинні бути заповнені");
+  } else {
+    const mail = formElement.email.value;
+    const password = formElement.password.value;
+
+    const formData = {
+      mail,
+      password,
+    };
+    console.log(formData);
+
+    event.target.reset();
+  }
+}
