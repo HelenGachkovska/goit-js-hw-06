80ec0c596af9f3f77c3b1d3605905f75a599151c
HelenGachkovska <ogachkovska@gmail.com>
2023-03-01 11:25:29 +0200

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+console.log(boxesEl);
+
+createBtn.addEventListener('click', getAmount);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  createBoxes(inputEl.value)
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  inputEl.value = '';
+}
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement('div');
+    size += i * 10;
+    boxEl.style.width = `${size}px`;
+    boxEl.style.height = `${size}px`;
+    boxEl.style.backgroundColor = getRandomHexColor();
+    // console.log(boxEl);
+
+    boxesEl.append(boxEl);
+  }
+}
